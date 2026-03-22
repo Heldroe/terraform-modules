4f89342d181041a241b559bb6abcc800c0a3e1d8
David Guerrero <heldroe@gmail.com>
2026-03-22 13:43:12 +0100

    Adding outputs to gateway module

---
+++ kubernetes/envoy/gateway/99-outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+  description = "The Envoy Gateway name."
+  value       = var.name
+}
+
+output "namespace" {
+  description = "The Gateway's namespace."
+  value       = var.namespace
+}
