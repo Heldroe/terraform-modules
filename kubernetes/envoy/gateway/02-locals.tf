f8d042dd6221aa41ccb38675fbcb56d81ed35747
David Guerrero <heldroe@gmail.com>
2026-03-28 11:06:13 +0100

    Fix envoy host container port logic

--- kubernetes/envoy/gateway/02-locals.tf
+++ kubernetes/envoy/gateway/02-locals.tf
@@ -20,6 +20,17 @@ locals {
     }
   }
 
+  # The first port in the ephemeral port range.
+  # https://github.com/envoyproxy/gateway/blob/fae7f1975fec116b48df9385ebbe49e750746cb2/internal/gatewayapi/translator.go#L45
+  min_ephemeral_port = 1024
+  # Constant added to the well known port (1-1023) to convert it into an ephemeral port.
+  # https://github.com/envoyproxy/gateway/blob/fae7f1975fec116b48df9385ebbe49e750746cb2/internal/gatewayapi/translator.go#L48
+  well_known_port_shift = 10000
+
+  # Determine envoy gateway container ports
+  http_container_port  = var.http_port < local.min_ephemeral_port ? var.http_port + local.well_known_port_shift : var.http_port
+  https_container_port = var.https_port < local.min_ephemeral_port ? var.https_port + local.well_known_port_shift : var.https_port
+
   host_network_patch = {
     patch = {
       value = {
@@ -31,14 +42,14 @@ locals {
                   name = "envoy"
                   ports = [
                     {
-                      containerPort = 10080
-                      hostPort      = 80
+                      containerPort = local.http_container_port
+                      hostPort      = var.http_port
                       name          = "http"
                       protocol      = "TCP"
                     },
                     {
-                      containerPort = 10443
-                      hostPort      = 443
+                      containerPort = local.https_container_port
+                      hostPort      = var.https_port
                       name          = "https"
                       protocol      = "TCP"
                     },
