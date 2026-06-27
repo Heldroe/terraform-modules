28ac42fb4b71b080871b28025c3b00340ba6fff9
David Guerrero <heldroe@gmail.com>
2026-06-27 13:16:25 +0200

    Update netbird access policy as it was flaky

--- netbird/config/40-policies.tf
+++ netbird/config/40-policies.tf
@@ -7,9 +7,18 @@ resource "netbird_policy" "clients_to_cluster" {
     bidirectional = false
     enabled       = true
     protocol      = "tcp"
-    ports         = ["80", "443"]
-    name          = "Clients to clusters access"
-    sources       = [for group in var.client_groups : netbird_group.group[group].id]
-    destinations  = [for group in keys(var.remote_cluster_groups) : netbird_group.group[group].id]
+    port_ranges = [
+      {
+        start = 80
+        end   = 80
+      },
+      {
+        start = 443
+        end   = 65535
+      },
+    ]
+    name         = "Clients to clusters access"
+    sources      = [for group in var.client_groups : netbird_group.group[group].id]
+    destinations = [for group in keys(var.remote_cluster_groups) : netbird_group.group[group].id]
   }
 }
