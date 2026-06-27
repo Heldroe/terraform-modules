73230864689b30e92e26bcbde1be452118a1ac69
David Guerrero <heldroe@gmail.com>
2026-06-27 18:34:34 +0200

    Netbird: Add additional routing for private ingress

--- netbird/config/20-routes.tf
+++ netbird/config/20-routes.tf
@@ -10,6 +10,21 @@ resource "netbird_route" "private_ingress" {
   access_control_groups = [for client_group in var.client_groups : netbird_group.group[client_group].id]
 }
 
+resource "netbird_route" "private_ingress_domain" {
+  for_each = var.remote_cluster_groups
+
+  network_id  = "${each.key} private ingress domain"
+  groups      = [for client_group in var.client_groups : netbird_group.group[client_group].id]
+  peer_groups = [netbird_group.group[each.key].id]
+  description = "Private ingress IPs for ${each.key}"
+  domains = [
+    each.value.private_ingress_domain,
+    "*.${each.value.private_ingress_domain}",
+  ]
+
+  access_control_groups = [for group in keys(var.remote_cluster_groups) : netbird_group.group[group].id]
+}
+
 resource "netbird_route" "kubernetes_api" {
   for_each = var.remote_cluster_groups
 
